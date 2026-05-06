732a2bdc3fdffedf7a797cf8476e5fb0a0dccf71
benjaminehaynes-prog <benjamin.e.haynes@gmail.com>
2026-05-06 10:14:51 -0400

    Added shapes, player, and asteroids. Collision is next

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 import pygame
+from player import Player
 from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
 from logger import log_state
+
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock =pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(("black"))
+        updatable.update(dt)
+        screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-
+        elapsed = clock.tick(60) / 1000
+        dt = elapsed
+        print(dt)
 
 if __name__ == "__main__":
     main()
