0b865ee23eaf0ec3df825a18c81e9d6e77da4ecf
benjaminehaynes-prog <benjamin.e.haynes@gmail.com>
2026-05-05 21:37:55 -0400

    version 1

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(("black"))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
